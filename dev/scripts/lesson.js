5783e0c62403d150c11bd46538cfdc9a82e9bab1
hitfm00 <hitfm00@gmail.com>
2023-04-28 22:01:16 +0300

    feat: added lesson page

---
+++ dev/scripts/lesson.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  var iframe = $("#lessonVideo");
+  var player = new Vimeo.Player(iframe);
+
+  // Get cid from query
+  var urlParams = new URLSearchParams(window.location.search);
+  var cid = urlParams.get("cid");
+
+  player.on("timeupdate", function(data) {
+    var currentTime = data.seconds;
+    var lastRequestTime = localStorage.getItem("lastRequestTime") || 0;
+
+    if (currentTime - lastRequestTime > 10) {
+      $.get(
+        `https://crm.code-ab.com/5/api/add_field?cid=${cid}&key=video_duration&value=${currentTime}`,
+        function(response) {
+          // обработка ответа
+          console.log(`Запрос отправлен ${currentTime}`)
+        },
+      );
+
+      localStorage.setItem("lastRequestTime", currentTime);
+    }
+  });
+});
